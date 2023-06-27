5eae52ee711851c31625a7d49307f5fe75c455bd
Aleksandr <medved-1994-09@mail.ru>
2023-06-27 21:56:33 +0700

    add project quiz 2.0

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ const questions = [
   {
     title: 'React - это ... ?',
     variants: ['библиотека', 'фреймворк', 'приложение'],
-    correct: 0
+    correct: [0]
   },
   {
     title: 'Компонент - это ... ',
     variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-    correct:  1
+    correct: [1]
   },
   {
     title: 'Что такое JSX?',
@@ -19,24 +19,24 @@ const questions = [
       'Это функция',
       'Это тот же HTML, но с возможностью выполнять JS-код',
     ],
-    correct: 2
-    
+    correct: [2]
+
   },
 ];
 
-function Result({correct}) {
+function Result({ correct }) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
       <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
       <a href='/'>
-      <button>Попробовать снова</button>
+        <button>Попробовать снова</button>
       </a>
     </div>
   );
 }
 
-function Game({step, question, onClickVariant }) {
+function Game({ step, question, onClickVariant, nextStep, focusButton }) {
 
   const percentage = Math.round(step / questions.length * 100)
 
@@ -48,9 +48,10 @@ function Game({step, question, onClickVariant }) {
       <h1>{question.title}</h1>
       <ul>
         {
-          question.variants.map((text, index) => <li key={text} onClick={() => onClickVariant(index)}>{text}</li>)
+          question.variants.map((text, index) => <li className={`${focusButton.includes(index) ? 'active' : ''}`} key={text} onClick={() => onClickVariant(index)}>{text}</li>)
         }
       </ul>
+      <button onClick={() => nextStep(step)}>Ответить</button>
     </>
   );
 }
@@ -59,15 +60,30 @@ function App() {
 
   const [step, setStep] = React.useState(0);
   const [correct, setCorrect] = React.useState(0);
+  const [focusButton, setFocusButton] = React.useState([]);
   const question = questions[step];
 
-  const onClickVariant = (index) => {
-    setStep(step + 1);
-    console.log(step, index)
+  const nextStep = () => {
 
-    if(index === question.correct) {
+    let arrWrongAnswere = focusButton.filter(num => !questions[step].correct.includes(num))
+    if (arrWrongAnswere.length == 0 && focusButton.length == questions[step].correct.length) {
       setCorrect(correct + 1);
+
     }
+
+    setStep(step + 1);
+    setFocusButton([]);
+  }
+
+  const onClickVariant = (index) => {
+
+    if (focusButton.includes(index)) {
+      setFocusButton(focusButton.filter(num => index !== num));
+    } else {
+      setFocusButton(prev => [...prev, index])
+      console.log(focusButton);
+    }
+
   }
 
 
@@ -75,12 +91,12 @@ function App() {
     <div className="App">
       {
         step !== questions.length ? (
-        <Game step={step} question={question} onClickVariant={onClickVariant} />
+          <Game step={step} question={question} onClickVariant={onClickVariant} nextStep={nextStep} focusButton={focusButton} />
         ) : (
-        <Result correct={correct}/>
+          <Result correct={correct} />
         )
       }
-     
+
     </div>
   );
 }
